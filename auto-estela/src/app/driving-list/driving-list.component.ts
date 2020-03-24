c7051d52321df53b9f56ab99e35aebd3d109b59b
AnthonyEstelaF <estela.flores.antony.rober.oswaldo@iisgalvanimi.edu.it>
2020-03-24 16:40:27 +0000

    punt6e7

--- auto-estela/src/app/driving-list/driving-list.component.ts
+++ auto-estela/src/app/driving-list/driving-list.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Drive } from '../driving.model';
 import { observable, Observable } from 'rxjs';
+import { Rent } from '../rent.model';
+
 @Component({
   selector: 'app-driving-list',
   templateUrl: './driving-list.component.html',
   styleUrls: ['./driving-list.component.css']
 })
 export class DrivingListComponent implements OnInit {
+  @Input() rentList: Rent[];
   obs_drive:Observable<Drive[]>;
   driveList: Drive[];
   selectedCar:Drive;
@@ -25,5 +28,6 @@ export class DrivingListComponent implements OnInit {
   {
     console.log(auto);
     this.selectedCar=auto;
+    this.rentList.push(new Rent(auto,1));
   }
 }
